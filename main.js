bb1da2c2c227daf0b2857aabc32f73eae71b9950
yuju <yuju99924@gmail.com>
2025-08-20 23:38:57 +0800

    新增aos動畫效果

--- main.js
+++ main.js
@@ -1,3 +1,20 @@
 import './assets/scss/all.scss';
 
 console.log('Hello world');
+
+document.addEventListener("DOMContentLoaded", function() {
+  if (window.innerWidth < 767) {
+    // 先移除指定區塊的 data-aos
+    document.querySelectorAll(".aos-md-disable [data-aos]").forEach(function(el) {
+      el.removeAttribute("data-aos");
+    });
+  }
+
+  // 再初始化 AOS
+  AOS.init({
+  offset: 120,
+  duration: 800,
+  once: false,
+  delay: 0,
+});
+});
